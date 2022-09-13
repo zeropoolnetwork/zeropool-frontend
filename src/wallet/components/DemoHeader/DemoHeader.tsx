33f777b0fdbd9569ecb404ec643c1bcc7a4a7e7d
Maksim Pegov <maksim.peg@gmail.com>
2022-09-13 10:35:35 +0200

    [ZP-UI-179] Fix demo heder popups

--- src/wallet/components/DemoHeader/DemoHeader.tsx
+++ src/wallet/components/DemoHeader/DemoHeader.tsx
@@ -29,54 +29,65 @@ export const DemoHeader: React.FC<DemoHeaderProps> = ({
   const { enqueueSnackbar } = useSnackbar()
   const handleAddressClick = (event: MouseEvent<HTMLSpanElement>): void => {
     const address = (event.target as HTMLElement).id === 'Public' ? walletAddress : privateAddress
+
     copyToClipboard((address as string), 'Address', enqueueSnackbar)
   }
+
+  const handleAmountClick = (event: MouseEvent<HTMLSpanElement>): void => {
+    const amount = (event.target as HTMLElement).ariaLabel
+
+    copyToClipboard((amount as string), 'Balance', enqueueSnackbar)
+  }
+
   return (
     <div className={bem()} data-testid={bem()}>
       <div className={bem('Amounts')}>
         <div>
           Public address:&nbsp;
-
-          <span id="Public" onClick={handleAddressClick} style={{ cursor: 'pointer' }}>
-            {beautifyAddress(walletAddress|| '')}
-          </span>
+          <Tooltip title={walletAddress || ''}>
+            <span id="Public" onClick={handleAddressClick} style={{ cursor: 'pointer' }}>
+              {beautifyAddress(walletAddress || '')}
+            </span>
+          </Tooltip>
         </div>
 
         <div>
           Private address:&nbsp;
 
-          <span id="Private" onClick={handleAddressClick} style={{ cursor: 'pointer' }}>
-            {beautifyAddress(privateAddress || '')}
-          </span>
+          <Tooltip title={privateAddress || ''}>
+            <span id="Private" onClick={handleAddressClick} style={{ cursor: 'pointer' }}>
+              {beautifyAddress(privateAddress || '')}
+            </span>
+          </Tooltip>
         </div>
 
         <div>
           Public balance:&nbsp;
 
-          <Tooltip title={publicBalance || ''} onClick={() => alert(publicBalance)}>
+          <Tooltip title={publicBalance || ''}>
             {publicBalance === undefined
               ? <CircularProgress className={bem('Progress')} color="inherit" />
-              : <span>{beautifyAmount(publicBalance)}</span>}
+              : <span onClick={handleAmountClick} style={{ cursor: 'pointer' }}>{beautifyAmount(publicBalance)}</span>}
           </Tooltip>
         </div>
 
         <div>
           Token balance:&nbsp;
 
-          <Tooltip title={tokenBalance || ''} onClick={() => alert(tokenBalance)}>
+          <Tooltip title={tokenBalance || ''}>
             {tokenBalance === undefined
               ? <CircularProgress className={bem('Progress')} color="inherit" />
-              : <span>{beautifyAmount(tokenBalance)}</span>}
+              : <span onClick={handleAmountClick} style={{ cursor: 'pointer' }}>{beautifyAmount(tokenBalance)}</span>}
           </Tooltip>
         </div>
 
         <div>
           Private balance:&nbsp;
 
-          <Tooltip title={privateBalance || ''} onClick={() => alert(privateBalance)}>
+          <Tooltip title={privateBalance || ''}>
             {privateBalance === undefined
               ? <CircularProgress className={bem('Progress')} color="inherit" />
-              : <span>{beautifyAmount(privateBalance)}</span>}
+              : <span onClick={handleAmountClick} style={{ cursor: 'pointer' }}>{beautifyAmount(privateBalance)}</span>}
           </Tooltip>
         </div>
       </div>
