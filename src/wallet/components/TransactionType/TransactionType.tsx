1197018b5d6a80fb27144408897bb705452b64d0
Maksim Pegov <maksim.peg@gmail.com>
2022-11-02 12:19:20 +0100

    [bugfix] Fix balance titles

--- src/wallet/components/TransactionType/TransactionType.tsx
+++ src/wallet/components/TransactionType/TransactionType.tsx
@@ -1,19 +1,20 @@
 import { cn } from '@bem-react/classname'
 import { TransferType } from 'shared/models'
+import { TransactionType as TrType } from 'shared/models/transaction'
 
 import './TransactionType.scss'
 
 const bem = cn('TransactionType')
 
 type TransferTypeProps = {
-   transferType: TransferType
+   transferType: TrType
 }
 
 export const TransactionType: React.FC<TransferTypeProps> = ({ transferType }) => {
 
-   const text = (transferType: TransferType) => {
+   const text = (transferType: TrType) => {
       let result
-      
+
       switch (transferType) {
          case 'funds':
             result = 'Funds'
